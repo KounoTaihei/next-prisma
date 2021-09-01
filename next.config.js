f94823ff1398f5f141296040f665eb31fcef0f89
河野太平 <>
2021-09-01 23:49:42 +0900

    configにenvの設定を追加

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  pageExtensions: ['tsx', 'api.ts']
+  pageExtensions: ['tsx', 'api.ts'],
+  env: {
+    API_URL: process.env.API_URL
+  }
 }
