790db6a3487e56e94b9c5479c4ca3bd735a8d825
河野太平 <>
2021-09-12 16:29:39 +0900

    ドロワーメニュー追加

--- layouts/header.tsx
+++ layouts/header.tsx
@@ -1,12 +1,16 @@
-import { AppBar, Avatar, Toolbar } from '@material-ui/core';
+import { AppBar, Avatar, Button, Drawer, IconButton, List, ListItem, Toolbar } from '@material-ui/core';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { useSession } from 'next-auth/client';
 import Link from 'next/link';
 import Image from 'next/image';
 import humanImage from '../public/human.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faBookOpen } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 const Header = () => {
     const [ session ] = useSession();
+
     const useStyles = makeStyles(() => 
         createStyles({
             appBar: {
@@ -19,37 +23,89 @@ const Header = () => {
             },
             avatar: {
                 border: "white 1px solid"
+            },
+            menu: {
+                width: "70vw"
+            },
+            menuItem: {
+                padding: "10px",
+                width: "auto"
             }
         })
     );
-    
     const classes = useStyles();
 
+    /** メニュー */
+    const [ menuOpening, setMenuOpening ] = useState<boolean>(false);
+
+    const menuContents = (
+        <List className={classes.menu}>
+            <Link href="/">
+                <ListItem
+                    button
+                    className={classes.menuItem}
+                    onClick={() => setMenuOpening(false)}
+                >
+                    <Button>
+                        Top
+                    </Button>
+                </ListItem>
+            </Link>
+            <Link href="/notes">
+            <ListItem
+                button
+                className={classes.menuItem}
+                onClick={() => setMenuOpening(false)}
+            >
+                <Button
+                    startIcon={<FontAwesomeIcon icon={faBookOpen} />}
+                >
+                    ノート一覧
+                </Button>
+            </ListItem>
+                </Link>
+        </List>
+    )
+    /** --- */
+
     return (
-        <AppBar position="static" className={classes.appBar}>
-            <Toolbar className={classes.toolBar}>
-                <nav >
-                    <Link href="/"><a className="text-white p-2">Top</a></Link>
-                    <Link href="/notes"><a className="text-white p-2">Notes</a></Link>
-                </nav>
-                <nav>
-                    {!session && (
-                        <>
-                            <Avatar>
-                                <Image alt="未ログインユーザーの画像" src={humanImage} layout="fill" loading="lazy" />
-                            </Avatar>
-                        </>
-                    )}
-                    {session && (
-                        <>
-                            <Avatar className={classes.avatar}>
-                                <Image alt="ログインユーザーの画像" src={session.user.image} layout="fill" loading="lazy" />
-                            </Avatar>
-                        </>
-                    )}
-                </nav>
-            </Toolbar>
-        </AppBar>
+        <>
+            <AppBar position="static" className={classes.appBar}>
+                <Toolbar className={classes.toolBar}>
+                    <nav>
+                        <IconButton
+                            color="inherit"
+                            onClick={() => setMenuOpening(true)}
+                        >
+                            <FontAwesomeIcon icon={faBars} />
+                        </IconButton>
+                    </nav>
+                    <nav>
+                        {!session && (
+                            <>
+                                <Avatar>
+                                    <Image alt="未ログインユーザーの画像" src={humanImage} layout="fill" loading="lazy" />
+                                </Avatar>
+                            </>
+                        )}
+                        {session && (
+                            <>
+                                <Avatar className={classes.avatar}>
+                                    <Image alt="ログインユーザーの画像" src={session.user.image} layout="fill" loading="lazy" />
+                                </Avatar>
+                            </>
+                        )}
+                    </nav>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                anchor="left"
+                open={menuOpening}
+                onClose={() => setMenuOpening(false)}
+            >
+                {menuContents}
+            </Drawer>
+        </>
     )
 }
 
