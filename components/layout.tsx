3ed24556215793bfa3be964cd9f85555cd29a7f6
KounoTaihei <moondust126@gmail.com>
2021-09-23 02:06:51 +0900

    トップスクロールボタンを配置

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,8 +1,37 @@
-import { ReactChild } from "react";
+import { ReactChild, useEffect, useState } from "react";
 import Header from "./header";
 import Head from 'next/head';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { animateScroll as scroll } from 'react-scroll';
+import styles from '../styles/Layout.module.scss';
 
 const Layout = ({ children }: Props) => {
+    const [ scrolled, setScrolled ] = useState<boolean>(false);
+
+    useEffect(() => {
+        const scrollCheck = () => {
+            if(1000 > window.scrollY) {
+                setScrolled(false);
+            } else {
+                setScrolled(true);
+            }
+        };
+        window.addEventListener('scroll', scrollCheck, {
+            capture: false,
+            passive: true,
+        });
+        scrollCheck();
+
+        return (() => {
+            window.removeEventListener('scroll', scrollCheck);
+        });
+    },[])
+
+    const scrollToTop = () => {
+        scroll.scrollToTop();
+    }
+
     return (
         <>
             <Head>
@@ -10,6 +39,9 @@ const Layout = ({ children }: Props) => {
             </Head>
             <Header />
             <main>{children}</main>
+            <div className={scrolled ? `${styles.scroll} ${styles.active}` : styles.scroll} onClick={scrollToTop}>
+                <FontAwesomeIcon icon={faChevronUp} />
+            </div>
         </>
     )
 }
